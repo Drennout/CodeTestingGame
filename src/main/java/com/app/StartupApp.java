41b82be10cf52e57162e0ef1b1067ee7c331062f
drennout <serega10lad@gmail.com>
2021-10-04 15:40:49 +0300

    Update Startup Class

--- src/main/java/com/app/StartupApp.java
+++ src/main/java/com/app/StartupApp.java
@@ -1,7 +1,13 @@
 package com.app;
 
+import com.app.game.Game;
+
+import java.util.Scanner;
+
 public class StartupApp {
     public static void main(String[] args) {
+        Game game = new Game();
 
+        game.start();
     }
 }
